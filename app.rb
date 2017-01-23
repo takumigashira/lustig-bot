3ebddb39314eb7d205378cc2dc5f7cdf63084985
Takumi Shimizugashira <takumigashira@gmail.com>
2017-01-23 16:09:16 +0000

    eight

--- app.rb
+++ app.rb
@@ -1,11 +1,34 @@
-require 'sinatra'
-require 'line/bot'
+require 'sinatra'   # gem 'sinatra'
+require 'line/bot'  # gem 'line-bot-api'
+
+class HTTPProxyClient
+  def http(uri)
+    proxy = URI(ENV['FIXIE_URL'])
+    proxy_class = Net::HTTP::Proxy(proxy.host, proxy.port, proxy.user, proxy.password)
+    http = proxy_class.new(uri.host, uri.port)
+    if uri.scheme == "https"
+      http.use_ssl = true
+    end
+    http
+  end
+
+  def get(url, header = {})
+    uri = URI(url)
+    http(uri).get(uri.request_uri, header)
+  end
+
+  def post(url, payload, header = {})
+    uri = URI(url)
+    http(uri).post(uri.request_uri, payload, header)
+  end
+end
 
 def client
-  @client ||= Line::Bot::Client.new { |config|
+  @client ||= Line::Bot::Client.new do |config|
     config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
     config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-  }
+    config.httpclient = HTTPProxyClient.new
+  end
 end
 
 post '/callback' do
@@ -17,6 +40,7 @@ post '/callback' do
   end
 
   events = client.parse_events_from(body)
+
   events.each { |event|
     case event
     when Line::Bot::Event::Message
@@ -26,11 +50,7 @@ post '/callback' do
           type: 'text',
           text: event.message['text']
         }
-        client.reply_message(event['replyToken'], message)
-      when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-        response = client.get_message_content(event.message['id'])
-        tf = Tempfile.open("content")
-        tf.write(response.body)
+        p client.reply_message(event['replyToken'], message)
       end
     end
   }
